475f9be7a96b4cd11d56fc534e50e3521b4f6a7d
Aki L <n9loak00@students.oamk.fi>
2020-11-09 21:18:41 +0200

    todo 5/6 - done

--- app/Controllers/Todo.php
+++ app/Controllers/Todo.php
@@ -2,14 +2,42 @@
 
 use App\Models\TodoModel;
 
-class Todo extends BaseController
-{
+class Todo extends BaseController {
+    public function __construct() {
+        $session = \Config\Services::session();
+        $session->start();
+    }
+
     public function index() {
+        if (!isset($_SESSION['user'])) {
+            return redirect('login');
+        }
         $model = new TodoModel();
         $data['title'] = 'Todo';
         $data['todos'] = $model->getTodos();
         echo view('templates/header',$data);
         echo view('todo/list',$data);
         echo view('templates/footer',$data);
+    }
+
+    public function create() {
+        $model = new TodoModel();
+
+        if (!$this->validate([
+            'title' => 'required|max_length[255]',
+        ])){
+            echo view('templates/header' , ['title' => 'Add new task']);
+            echo view('todo/create');
+            echo view('templates/footer');
+        }
+        else {
+            $user = $_SESSION['user'];
+            $model->save([
+                'title' => $this->request->getVar('title'),
+                'description' => $this->request->getVar('description'),
+                'user_id' => $user->id
+            ]);
+            return redirect('todo');
+        }
     }
 }
--- todo.sql
+++ todo.sql
@@ -1,12 +1,24 @@
-DROP DATABASE IF EXISTS todo;
+drop database if exists todo;
 
-CREATE DATABASE todo;
+create database todo;
 
 use todo;
 
-CREATE TABLE task (
-	id int PRIMARY KEY AUTO_INCREMENT,
+create table user (
+    id int primary key auto_increment,
+    username varchar(30) not null unique,
+    password varchar(255) not null,
+    firstname varchar(100),
+    lastname varchar(100)
+);
+
+create table task (
+	id int primary key auto_increment,
     title varchar(255) not null,
-    added timestamp DEFAULT CURRENT_TIMESTAMP,
-    description text
+    added timestamp default current_timestamp,
+    description text,
+    user_id int not null,
+    index (user_id),
+    foreign key (user_id) references user(id)
+    on delete restrict
 )
